94758b719fad59a0e63b80274ad38db7d427bcad
Laura Tissi <laura.tracierra@gmail.com>
2021-06-10 21:02:12 -0300

    pegando informaçoes de quando o participante entra na sala e enviando o status na mensagem

--- src/server.js
+++ src/server.js
@@ -1,12 +1,36 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/participants", (req, res) => {
+let nextParticipantId = 1;
+let participants = [];
+let nextMessageId = 1;
+let messages = [];
 
+app.post("/participants", (req, res) => {
+    const participant = req.body;
+    const { name } = participant;
+    if(name.length === 0){
+        res.sendStatus(400);
+        return;
+    }
+    participant.lastStatus = Date.now();
+    participant.id = nextParticipantId;
+    participants.push(participant);
+    nextParticipantId++;
+    const enter = {
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs(participant.lastStatus).format('HH:mm:ss')
+    }
+    messages.push(enter);
+    res.sendStatus(200);
 });
 
 app.get("/participants", (req, res) => {
